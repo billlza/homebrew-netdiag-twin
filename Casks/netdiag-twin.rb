a0a556428c720ef62f55daa48064e03b87745b67
NetDiag Twin Release <release@netdiag-twin.invalid>
2026-09-13 10:30:15 +0000

    Release NetDiag Twin v0.5.4

--- Casks/netdiag-twin.rb
+++ Casks/netdiag-twin.rb
@@ -1,6 +1,6 @@
 cask "netdiag-twin" do
-  version "0.5.2"
-  sha256 "3577849201918af5c9a7e0e94e7bdd96ac8739146a4ba1d043080680ea2ea397"
+  version "0.5.4"
+  sha256 "67a9632a64117b1fc00fee849c58b89c51191a662ff4488a677034cb6213a02d"
 
   url "https://github.com/billlza/netdiag-twin/releases/download/v#{version}/NetDiag-Twin-#{version}.dmg"
   name "NetDiag Twin"
